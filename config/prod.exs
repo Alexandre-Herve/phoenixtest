278f30f97292e7b600453dc37c5c35e231d2035a
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-05-19 18:06:00 +0200

    Update config prod

--- config/prod.exs
+++ config/prod.exs
@@ -2,23 +2,22 @@ use Mix.Config
 
 config :app, App.Web.Endpoint,
   http: [port: {:system, "${PORT}"}],
+  # on_init: {App.Web.Endpoint, :load_from_system_env, []},
+  secret_key_base: System.get_env("${SECRET_KEY_BASE}"),
+  server: true,
   url: [
-    host: {:system, "${APP_URL}"},
+    host: {:system, "${HOST}"},
+    port: {:system, "${PORT}"}
     scheme: "http"
-  ],
-  on_init: {App.Web.Endpoint, :load_from_system_env, []},
-  server: true
+  ]
 
 config :logger, level: :info
 
-config :app, App.Web.Endpoint,
-  secret_key_base: System.get_env("${SECRET_KEY_BASE}")
-
 config :app, App.Repo,
   adapter: Ecto.Adapters.Postgres,
+  database: "${RDS_DB_NAME}",
   username: "${RDS_USERNAME}",
   password: "${RDS_PASSWORD}",
-  database: "${RDS_DB_NAME}",
   hostname: "${RDS_HOSTNAME}",
   port: "${RDS_PORT}" || 5432,
   ssl: true,
